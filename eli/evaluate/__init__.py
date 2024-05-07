00ce2a9b741d7ff32167fa8257886a80e9bf1a38
nicolai <nicolaibthomsen@gmail.com>
2024-05-07 07:36:42 +0200

    v1

--- eli/evaluate/__init__.py
+++ eli/evaluate/__init__.py
@@ -1,18 +1,29 @@
 import weave
 import editdistance
 
-from eli.utils.loggers import logger
+from utils.loggers import logger
+
 
 def postprocess(string):
-    return string.strip().lower()
+    return string.strip().lower().strip("answer:").strip()
 
 @weave.op()
 def evaluator(target: str, model_output: str) -> dict:
-    model_output = postprocess(model_output)
-    correct = target == model_output
+    """ Evaluate the model output against the target. We
+    consider the model output correct if it exactly matches
+    the target or if the edit distance between the two
+    strings is less than 2."""
 
-    logger.info(f"Target: {target} | Model: {model_output} | Correct: {correct}")
+    model_output = postprocess(model_output)
+    edit_distance = editdistance.eval(target, model_output)
+    
+    exact_match = target == model_output
+    fuzzy_match = edit_distance < 2
+    correct =  exact_match | fuzzy_match
+
+    logger.info(f"{target=} | {model_output=} | {correct=}")
     return {
         'correct': correct,
-        'edit_distance': editdistance.eval(target, model_output)
+        'edit_distance': edit_distance,
+        'exact_match': exact_match,
     }
--- eli/templates/__init__.py
+++ eli/templates/__init__.py
@@ -1,10 +1,12 @@
-from templates.center_embed import CE_TEMPLATE
+import templates.center_embed as CE
 from templates.default import DEFAULT_TEMPLATE
 from templates.supervised_cot import SUPERVISED_COT_TEMPLATE
 from templates.unsupervised_cot import UNSUPERVISED_COT_TEMPLATE
 
 PROMPT_TEMPLATE_REGISTRY = {
-    "center_embed": CE_TEMPLATE,
+    "center_embed": CE.CE_TEMPLATE,
+    "center_embed_tn1": CE.CE_TEMPLATE_Tn1,
+    "center_embed_tn2": CE.CE_TEMPLATE_Tn2,
     "default": DEFAULT_TEMPLATE,
     "supervised-cot": SUPERVISED_COT_TEMPLATE,
     "unsupervised-cot": UNSUPERVISED_COT_TEMPLATE
